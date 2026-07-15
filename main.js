15f45445aefe975c9e46b1a55636237cec215bcf
ASADBEK <asadbekaxmd@gmail.com>
2026-07-15 08:37:30 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+
+let = a = 111 , b = 777  
+
+console.log(a + b); 
+
+
+let names = "asadbek"
+
+let numbers = 123456
+
+let  Boolean = 'folse'
+
+
+console.log(  names , numbers , Boolean);
+
+
+
+
+
+
+
+
